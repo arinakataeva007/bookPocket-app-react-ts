6df11fba2ef4b3d5ff9fa0f1dd34785400d19b6f
Arina Kataeva <123296525+arinakataeva007@users.noreply.github.com>
2024-10-04 19:53:02 +0500

    feat(feat): создание прилжения

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Testpage from './components/test.page';
+function App() {
+  return (
+    <div className="App">
+      <Testpage  message='Hello'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/test.page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+const Testpage = ({message}: any) => {
+    return (
+        <div>{message}</div>
+    );
+}
+export default Testpage;
